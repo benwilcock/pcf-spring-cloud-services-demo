9950296a9f1ef00793625ddbaec965a2398d2a2d
benwilcock <benwilcock@gmail.com>
2016-11-29 11:30:29 +0000

    added some helpful comments in the code and the config to help trace where the requirement for each comes from

--- setup-services.sh
+++ setup-services.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+cf cs p-rabbitmq standard rabbit
 cf cs p-circuit-breaker-dashboard standard breaker
 cf cs p-service-registry standard registry
 cf cs p-config-server standard config -c config-server-setup.json
--- zipkin-server/src/main/java/io/pivotalservices/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/io/pivotalservices/ZipkinServerApplication.java
@@ -5,8 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@EnableDiscoveryClient
-@EnableZipkinStreamServer
+
+/**
+ * As a Spring Boot application, much of the configuration is dependent on the
+ * classpath dependencies such as Sleuth, Stream, Rabbit etc. See the `build.gradle`
+ * for details.
+ **/
+@EnableDiscoveryClient // Enables Auto registration with the [Registry]
+@EnableZipkinStreamServer // Enables [Zipkin] Server and allows Stream collection
 @SpringBootApplication
 public class ZipkinServerApplication {
 
